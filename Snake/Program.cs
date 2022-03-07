95f53243b60f43b647b275357519a0f881140f49
EraserHead <makc200690@gmail.com>
2022-03-07 16:29:32 +0300

    lessen-9 Snake

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,17 +8,19 @@ namespace Snake
         static void Main(string[] args)
         {          
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Drow();
+            //Point p1 = new Point(1, 3, '*');
+            //p1.Drow();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Drow();
+            //HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            //line.Drow();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+            //VerticalLine lineV = new VerticalLine(7, 12, 10, '$');
+            //lineV.Drow();
 
-            VerticalLine lineV = new VerticalLine(7, 12, 10, '$');
-            lineV.Drow();
+
+            Point p = new Point(4, 5, '~');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }      
