e9c664c18c10309694a6eb6c9a39f1cd823f14c9
EraserHead <makc200690@gmail.com>
2022-03-07 14:02:38 +0300

    lesson-7 horizontalLine

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,27 +14,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            numList.Add(3);
-            numList.Add(4);
-            numList.Add(5);
-            numList.Add(7);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine();
+            line.Drow();
 
             Console.ReadLine();
         }      
